061673912bed16f7c91c13e2b3fa6de9ef040990
Stefany Noguera <stefanynoguerar@gmail.com>
2023-05-23 12:27:46 -0500

    resume page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
         <section className="min-h-screen">
 
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl dark:text-gray-300">Stefany Noguera</h1>
+            <h1 className="text-lg dark:text-gray-300">Stefany Noguera</h1>
             <ul className="flex items-center">
-              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-gray-100"/></li>
-              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+              <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-xl dark:text-gray-100"/></li>
+              <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="/resume">Resume</a></li>
             </ul>
           </nav>
 
@@ -29,14 +29,14 @@ export default function Home() {
           </div>
 
           <div className="text-5xl flex justify-center gap-16 pt-3 text-gray-600 dark:text-gray-100">
-            <a href="#"><AiFillTwitterCircle/></a>
-            <a href="#"><AiFillGithub/></a>
-            <a href="#"><AiFillLinkedin/></a>
+            {/* <a href="#"><AiFillTwitterCircle/></a> */}
+            <a href="https://github.com/StefanyNoguera"><AiFillGithub/></a>
+            <a href="https://www.linkedin.com/in/stefanynoguera/"><AiFillLinkedin/></a>
           </div>
 
           <div className="relative mx-auto rounded-full m-20 flex justify-center h-80 w-80 md:h-96 md:w-96">
             <Image
-              src="/camila.jpeg"
+              src="/stefany.jpg"
               fill="responsive"
               object-fit= "cover"
               className="rounded-full"
@@ -127,7 +127,7 @@ export default function Home() {
             </p>
           </div>
 
-          <div className= "flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+          <div className= "flex flex-col gap-10 py-10 items-center lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1">
               <Image
                 src="/web1.png"
